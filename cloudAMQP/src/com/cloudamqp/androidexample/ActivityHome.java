536a159848ffd1eeedc698df07a615f9eff388fa
Lovisa Johansson <lovisa@84codes.com>
2014-10-09 16:05:41 +0800

    Formatting

--- cloudAMQP/src/com/cloudamqp/androidexample/ActivityHome.java
+++ cloudAMQP/src/com/cloudamqp/androidexample/ActivityHome.java
@@ -9,57 +9,58 @@ import android.util.Log;
 
 public class ActivityHome extends Activity {
 	private Connection connection;
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	  super.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
 
-	  Thread thread = new Thread(new Runnable(){
-	    @Override
-	    public void run() {
-	      String uri = "CLOUDAMQP_URL";
-	      try {
-	        // Open a connection
-	        ConnectionFactory factory = new ConnectionFactory();
-	        factory.setUri(uri);
-	        connection = factory.newConnection();  
+		Thread thread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				String uri = "CLOUDAMQP_URL";
+				try {
+					// Open a connection
+					ConnectionFactory factory = new ConnectionFactory();
+					factory.setUri(uri);
+					connection = factory.newConnection();
 
-	        Channel channel1 = connection.createChannel();
-	        Channel channel2 = connection.createChannel();
+					Channel channel1 = connection.createChannel();
+					Channel channel2 = connection.createChannel();
 
-	        String message =  "Hello CloudAMQP!";
-	        
-	        // Declare the queue 'hello' 
-	        channel1.queueDeclare("hello", false, false, false, null);
+					String message = "Hello CloudAMQP!";
 
-	        QueueingConsumer consumer = new QueueingConsumer(channel2);
-	        channel2.basicConsume("hello", true, consumer);
+					// Declare the queue 'hello'
+					channel1.queueDeclare("hello", false, false, false, null);
 
-	        // Publish the message 'Hello CloudAMQP''
-	        channel1.basicPublish("", "hello", null, message.getBytes());
-	        Log.d("", " [x] Sent '" + message + "'");
+					QueueingConsumer consumer = new QueueingConsumer(channel2);
+					channel2.basicConsume("hello", true, consumer);
 
-	        while (true) {
-	          QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-	          String message1 = new String(delivery.getBody());
-	          Log.d("", " [x] Received '" + message1 + "'");
-	        }
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      }
-	    }
-	  });
-	  thread.start();
+					// Publish the message 'Hello CloudAMQP''
+					channel1.basicPublish("", "hello", null, message.getBytes());
+					Log.d("", " [x] Sent '" + message + "'");
+
+					while (true) {
+						QueueingConsumer.Delivery delivery = consumer
+								.nextDelivery();
+						String message1 = new String(delivery.getBody());
+						Log.d("", " [x] Received '" + message1 + "'");
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		thread.start();
 	}
 
-    @Override
-    protected void onResume() {
-        super.onPause();
-    }
- 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
- 
+	@Override
+	protected void onResume() {
+		super.onPause();
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+
 }
