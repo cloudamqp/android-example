6f5402ccdc511c39a62328dc5913b28ccdb7ffca
Lovisa Johansson <lovisa@84codes.com>
2014-10-22 23:19:36 +0800

    Removed services

--- cloudAMQP/src/com/cloudamqp/androidexample/ActivityHome.java
+++ cloudAMQP/src/com/cloudamqp/androidexample/ActivityHome.java
@@ -1,21 +1,25 @@
 package com.cloudamqp.androidexample;
+import java.net.URISyntaxException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 
 
 import com.cloudamqp.R;
-import com.cloudamqp.androidexample.MyService.LocalBinder;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.AMQP.Queue.DeclareOk;
 
 
 import android.app.Activity;
 
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
@@ -27,98 +31,175 @@ import android.widget.TextView;
 
 public class ActivityHome extends Activity {
 
-	MyService mService;
-	boolean mBound = false;
-	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+		Log.d("","on create");
 		setContentView(R.layout.activity_main);
-		
+
 		Button button = (Button) findViewById(R.id.publish);
 		button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-				if(mBound)
-				{
-					EditText et = (EditText) findViewById(R.id.text);
-					mService.publishMessage(et.getText().toString());
-					et.setText("");
-				}
+				EditText et = (EditText) findViewById(R.id.text);
+				publishMessage(et.getText().toString());
+				et.setText("");
 			}
 		});
+		
+		Handler handler = new Handler() {
+			@Override
+			public void handleMessage(Message msg) {
+				String message=msg.getData().getString("msg");
+				TextView tv = (TextView) findViewById(R.id.textView);
+				Date dNow = new Date( );
+				SimpleDateFormat ft = new SimpleDateFormat ("hh:mm:ss");
+				tv.append(ft.format(dNow) + ' ' + message + '\n');
+			}
+		};
+		setupConnectionFactory();		
+		publishToAMQP();
+		subscribe(handler);
 	}
-	
+
 	@Override
 	protected void onStart() {
 		super.onStart();
-		Log.d("","onstart");
-		Intent intent = new Intent(this, MyService.class);
-		bindService(intent, mConnection, Context.BIND_AUTO_CREATE);		
-		
-	}
+		Log.d("","on start");
 
-	@Override
-	protected void onStop() {
-		super.onStop();
-		Log.d("","onStop");
-		mService.unsubscribe();  
-		// Unbind from the service
-		if (mBound) {
-			unbindService(mConnection); 
-			mBound = false;
-		}
-	}
-
-	@Override
-	protected void onResume() {
-		super.onResume();
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
 	}
 
 	@Override
 	protected void onDestroy() {
-		
 		super.onDestroy();
-		Log.d("","onDestroy");
-		
-		
+		unsubscribe();
 	}
-	/** Defines callbacks for service binding, passed to bindService() */
-	private ServiceConnection mConnection = new ServiceConnection() {
+	
+	ConnectionFactory factory = new ConnectionFactory();
+	private BlockingDeque<String> queue = new LinkedBlockingDeque<String>();
 
-		@Override
-		public void onServiceConnected(ComponentName className,
-				IBinder service) {
-			// We've bound to LocalService, cast the IBinder and get LocalService instance
-			LocalBinder binder = (LocalBinder) service;
-			mService = binder.getService();
-			mBound = true;
+	public void publishMessage(String message) {
+		//Adds a message to internal blocking queue
+		try {
+			Log.d("","[q] " + message);
+			queue.putLast(message);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 
-			Handler handler = new Handler() {
-				@Override
-				public void handleMessage(Message msg) {
-					String message=msg.getData().getString("msg");
-					TextView tv = (TextView) findViewById(R.id.textView);
-					Date dNow = new Date( );
-					SimpleDateFormat ft = new SimpleDateFormat ("hh:mm:ss"); 
-					tv.append(ft.format(dNow) + ' ' + message + '\n');
+	private void setupConnectionFactory() {
+		String uri = "CLOUDAMQP_URL";
+		try {
+			factory.setAutomaticRecoveryEnabled(false);	
+			factory.setUri(uri);
+		} catch (KeyManagementException | NoSuchAlgorithmException	| URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	private Connection connect() throws Exception
+	{
+		Connection connection = factory.newConnection();
+		Log.d("", "Connection established");
+		return connection; 
+	}
+
+	public void subscribe(final Handler handler)
+	{
+		subscribeThread = new Thread(new Runnable() { 
+			@Override
+			public void run() {
+				while(true)
+				{
+					try{
+						Connection connection = connect();
+						Channel channel = connection.createChannel();
+						channel.basicQos(1);
+						DeclareOk q = channel.queueDeclare();
+						channel.queueBind(q.getQueue(), "amq.fanout", "chat");
+						QueueingConsumer consumer = new QueueingConsumer(channel);
+						channel.basicConsume(q.getQueue(), true, consumer);
+
+						// Process deliveries
+						while (true) {
+							QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+
+							String message = new String(delivery.getBody());
+							Log.d("","[r] "+message);
+
+							Message msg = handler.obtainMessage();
+							Bundle bundle = new Bundle();
+
+							bundle.putString("msg", message);
+							msg.setData(bundle);
+							handler.sendMessage(msg);
+						}
+					} catch (InterruptedException e) {
+						break;
+					} catch (Exception e1) {
+						Log.d("","Connection broken"+ e1.getClass().getName());
+						try {
+							//sleep and then try again
+							Thread.sleep(4000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+					}
 				}
-			};
-			mService.subscribe(handler);
-		}
+			}
+		});
+		subscribeThread.start();
+	}
 
-		@Override
-		public void onServiceDisconnected(ComponentName arg0) {
-			 mService = null;
-	         mBound = false;
-		}
-	};
+	Thread subscribeThread;
+	Thread publishThread;
+	public void unsubscribe()
+	{
+		publishThread.interrupt();
+		subscribeThread.interrupt();
+	}
 
+	public void publishToAMQP()
+	{
+		publishThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(true)
+				{
+					try {
+						Connection connection = connect();
+						Channel channel1 = connection.createChannel();
+						channel1.confirmSelect();
+
+						while (true) {
+							String message = queue.takeFirst();
+							try{
+								channel1.basicPublish("amq.fanout", "chat", null, message.getBytes());
+								Log.d("", "[s] " + message);
+								channel1.waitForConfirmsOrDie();
+
+							}catch (Exception e)
+							{
+								Log.d("","[f] " + message);
+								queue.putFirst(message);
+								throw e;
+							}
+						}
+					} catch (InterruptedException e) {
+						break;
+					} catch (Exception e) {
+						Log.d("","Connection broken");
+						try {
+							//sleep and then try again
+							Thread.sleep(5000);
+						} catch (InterruptedException e1) {
+							e1.printStackTrace();
+						}
+					}
+				}
+			}
+		});
+		publishThread.start();
+	}
 }
-
