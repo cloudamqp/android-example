19a279fe8a2cd107613f17a15020f60e7538771d
Carlos Luis Rojas Aragonés <crojasaragonez@hotmail.com>
2016-10-11 19:35:13 -0600

    Carl Hörberg comments
    - Removing Bus
    - Reuse amqp connections

--- Androidexample/app/src/main/java/com/cloudamqp/androidexample/ActivityHome.java
+++ Androidexample/app/src/main/java/com/cloudamqp/androidexample/ActivityHome.java
@@ -2,24 +2,16 @@ package com.cloudamqp.androidexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Consumer;
 import com.rabbitmq.client.DefaultConsumer;
 import com.rabbitmq.client.Envelope;
-import com.squareup.otto.Subscribe;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.TimeoutException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,20 +19,17 @@ import butterknife.OnClick;
 
 public class ActivityHome extends AppCompatActivity {
 
-    private static final String EXCHANGE_NAME = "chat";
-
-    @BindView(R.id.publish) Button button;
     @BindView(R.id.text) EditText et;
     @BindView(R.id.textView) TextView tv;
-    Thread subscribeThread;
-    Thread publishThread;
+
+    private Thread subscribeThread;
+    private Thread publishThread;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
-        BusProvider.getInstance().register(this);
         this.subscribe();
     }
 
@@ -51,23 +40,13 @@ public class ActivityHome extends AppCompatActivity {
         publishThread.interrupt();
     }
 
-    @Subscribe
-    public void messageAvailable(final String text) {
+    private void onMessageReceived(final String text) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                tv.append(text + "\n");
+                tv.append(text + System.getProperty("line.separator"));
             }
         });
-    }
-
-    private Channel getChannel() throws IOException, TimeoutException, NoSuchAlgorithmException, KeyManagementException, URISyntaxException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setUri(Constants.CLOUDAMQP_URI);
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-        channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
-        return channel;
     }
 
     @OnClick(R.id.publish)
@@ -78,8 +57,8 @@ public class ActivityHome extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    Channel channel = getChannel();
-                    channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes());
+                    Channel channel = AmqpProvider.getInstance().getWriteChannel();
+                    channel.basicPublish(Constants.EXCHANGE_NAME, "", null, message.getBytes());
                 } catch (Exception e) {
                     e.getMessage();
                     e.printStackTrace();
@@ -94,14 +73,14 @@ public class ActivityHome extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    Channel channel = getChannel();
+                    Channel channel = AmqpProvider.getInstance().getReadChannel();
                     String queueName = channel.queueDeclare().getQueue();
-                    channel.queueBind(queueName, EXCHANGE_NAME, "");
+                    channel.queueBind(queueName, Constants.EXCHANGE_NAME, "");
                     Consumer consumer = new DefaultConsumer(channel) {
                         @Override
                         public void handleDelivery(String consumerTag, Envelope envelope,
                                                    AMQP.BasicProperties properties, byte[] body) throws IOException {
-                            BusProvider.getInstance().post(new String(body, "UTF-8"));
+                            onMessageReceived(new String(body, "UTF-8"));
                         }
                     };
                     channel.basicConsume(queueName, true, consumer);
--- Androidexample/app/src/main/java/com/cloudamqp/androidexample/Constants.java
+++ Androidexample/app/src/main/java/com/cloudamqp/androidexample/Constants.java
@@ -4,6 +4,8 @@ package com.cloudamqp.androidexample;
  * Created by carlos on 10/7/16.
  */
 
-public class Constants {
+class Constants {
+    public static final String EXCHANGE_NAME = "chat";
+    public static final String EXCHANGE_TYPE = "fanout";
     public static final String CLOUDAMQP_URI = "amqp://user:password@host/vhost";
 }
